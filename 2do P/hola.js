ea5f4a88fde55cd9efd006ef0df13d782bc9c6ed
Jonathan Díaz <jonathandz2002@hotmail.com>
2026-05-22 07:20:19 -0500

    Inicio 2do P y apuntes js

---
+++ 2do P/hola.js
@@ -0,0 +1,40 @@
+/*console.log("Hello, World! ya quiero que acabe el semestre");
+let num1 = Number(prompt("ingrese número: "));
+let num2 = Number(prompt("ingrese número: "));
+console.log("La suma es: " + (num1 + num2));
+console.log("La resta es: " + (num1 - num2));
+console.log("La multiplicación es: " + (num1 * num2));
+console.log("La división es: " + (num1 / num2)));
+
+let base = Number(prompt("Ingrese la base del triángulo: "));
+let altura = Number(prompt("Ingrese la altura del triángulo: "));
+console.log("El área del triángulo es: " + (0.5 * base * altura));
+let lado1 = Number(prompt("Ingrese el primer lado del triángulo: "));
+let lado2 = Number(prompt("Ingrese el segundo lado del triángulo: "));
+let lado3 = Number(prompt("Ingrese el tercer lado del triángulo: "));
+let perimetro = lado1 + lado2 + lado3;
+console.log("El perímetro del triángulo es: " + perimetro);
+
+
+let nombre = prompt("Ingrese su nombre: ");
+let peso = Number(prompt("Ingrese su peso en kg: "));
+let altura = Number(prompt("Ingrese su altura en metros: "));
+let imc = peso / (altura * altura);
+console.log("Nombre: " + nombre);
+console.log("Peso: " + peso + " kg");
+console.log("Altura: " + altura + " m");
+console.log("Índice de masa corporal: " + imc.toFixed(2));
+if (imc < 18.5) {
+    console.log("Categoría: Bajo peso");
+} else if (imc >= 18.5 && imc < 25) {
+    console.log("Categoría: Peso normal");
+} else if (imc >= 25 && imc < 30) {
+    console.log("Categoría: Sobrepeso");
+} else {
+    console.log("Categoría: Obesidad");
+}
+*/
+
+let a=5
+let b="5"
+console.log(`${a==b}`)
