9f8dcbca3d0749dc7be427adc8ad9aa3e90ad2f5
Jonathan Díaz <jonathandz2002@hotmail.com>
2026-06-10 07:53:58 -0500

    Ejercicio Manejo de errores

---
+++ 2do P/ManejoErrores/manejoErrores.js
@@ -0,0 +1,59 @@
+try{
+    let usuario = "Jonathan";
+    console.log(usuario);
+
+} catch(error){
+    console.log(error.name);
+    console.log(error.message);
+} finally{
+    console.log("Fin del proceso");
+}
+
+/*
+//Ejercicio validar edad
+let edad = Number(prompt("Ingrese su edad: "));
+
+function validarEdad(edad){
+    if(edad < 18){
+        throw "Edad no valida"; 
+    } else {
+        console.log("Edad valida");
+    }
+}
+
+try {
+    validarEdad(edad);
+} catch(error) {
+    console.log(error);
+}
+*/
+
+/*
+Ejercicio 1
+Crear una funcion que valide una nota.
+
+Reglas:
+- 0 a 10 valido
+- Menor a 0 error
+- Mayor a 10 error
+
+Salida:
+Notas entre 0 y 10: Nota valida
+Notas menores a 0 y mayores a 10: Nota invalida
+*/
+
+let nota = Number(prompt("Ingrese una nota: "));
+
+function validarNota(nota){
+    if(nota < 0 || nota > 10){
+        throw"Nota invalida";
+    } else {
+        console.log("Nota valida");
+    }
+}
+
+try {
+    validarNota(nota);
+} catch (error) {
+    console.log(error);
+}
