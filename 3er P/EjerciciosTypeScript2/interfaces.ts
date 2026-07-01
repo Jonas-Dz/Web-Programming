ae9b6c0f678b063d6e7a6729278068482836f0e2
Jonathan Díaz <jonathandz2002@hotmail.com>
2026-07-01 07:42:57 -0500

    Propiedades opcionales

--- 3er P/EjerciciosTypeScript2/interfaces.ts
+++ 3er P/EjerciciosTypeScript2/interfaces.ts
@@ -21,3 +21,37 @@ let docente2:Docente = {
 
 console.log(docente1.nombre);
 console.log(docente2.nombre);
+
+/*
+//PROPIEDADES OPCIONALES
+
+interface Docente{
+    nombre:string,
+    asignatura:string,
+    horas?:number,  //si no se define, se asume que es undefined
+    activo:boolean
+};
+
+const docente1:Docente = {
+    nombre: "Juan",
+    asignatura: "Matemáticas",
+    activo: true
+};
+
+const docente2:Docente = {
+    nombre: "Andy",
+    asignatura: "BDD",
+    horas: 10,
+    activo: true
+};
+*/
+
+//Si las joras son mayores o iguales a 8 {es tiempo completo} sino {es tiempo parcial}
+
+console.log(docente1.nombre);
+console.log(docente1.horas);
+
+console.log(docente2.nombre);
+console.log(docente2.horas);
+
+console.log(docente1.horas >= 8 ? "Tiempo completo" : "Tiempo parcial");
