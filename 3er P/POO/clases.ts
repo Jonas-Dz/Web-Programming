2101a20d842d5266286bd1ab23b9d21ab97ad4b3
Jonathan Díaz <jonathandz2002@hotmail.com>
2026-07-03 08:04:42 -0500

    POO en TypeScript

---
+++ 3er P/POO/clases.ts
@@ -0,0 +1,17 @@
+class Persona{
+    nombre:string;
+    constructor(nombre:string){
+        this.nombre=nombre;
+    }
+}
+
+class Docente extends Persona{
+    mostrarNombre():void{
+        console.log('Docente: ${this.nombre}')
+    }
+}
+
+const docente = new Docente('Juan');
+docente.mostrarNombre();
+
+console.log(docente.nombre);
---
+++ 3er P/POO/herencia.ts
@@ -0,0 +1,30 @@
+class Persona{
+    nombre:string;
+    edad:number;
+
+    constructor(nombre:string, edad:number){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+
+    saludar():void{
+        console.log('Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años'); 
+    }
+}
+
+class Estudiante extends Persona{
+    carrera:string;
+
+    constructor(nombre:string, edad:number, carrera:string){
+        super(nombre, edad);
+        this.carrera=carrera;
+    }
+
+    mostrarCarrera():void{
+        console.log('Estudio ${this.carrera}');
+    }
+}
+
+const estudiante = new Estudiante('Jonathan', 24, 'Software');
+estudiante.saludar();
+estudiante.mostrarCarrera();
