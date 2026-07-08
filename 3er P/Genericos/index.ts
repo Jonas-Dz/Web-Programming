1f5173ebd76cbef292d3075b0a63dec4e3f43d05
Jonathan Díaz <jonathandz2002@hotmail.com>
2026-07-08 07:52:30 -0500

    Tipos de ejercicios con genericos

--- 3er P/Genericos/index.ts
+++ 3er P/Genericos/index.ts
@@ -1,7 +1,53 @@
 function mostrarDatos<T>(dato:T):void{
 	console.log(dato);
 }
+
 //any
-mostrarDato<string>("Hola");
-mostrarDato<number>(100);
-mostrarDato<boolean>(true);
+mostrarDatos<string>("Hola");
+mostrarDatos<number>(100);
+mostrarDatos<boolean>(true);
+
+/*
+//Funcion báscica con genericos
+function obtenerPrimero<T>(elemento:T[]):T{
+	return nelementos[0];
+}
+
+const primerNombre = obtenerPrimero<string>(["Ana", "Carlos", "Maria"]);
+const primerNumero = obtenerPrimero<number>([10 ,20, 30]);
+console.log(primerNombre);
+console.log(primerNumero);
+*/
+
+/*
+//Genericos con arreglos
+interface Estudiante{
+	nombre:string;
+	nota:number;
+}
+
+function mostrarLista<T>(elementos:T[]):void{
+	for(const elemento of elementos){
+		console.log(elemento);
+	}
+}
+
+const estudiantes:Estudiantes[]=[
+	{nombre: "Ana", nota: 18},
+	{nombre: "Carlos", nota: 15}
+];
+
+//Genericos con restricciones
+interface ConCodigo{
+	codigo:string;
+}
+
+function mostrarCodigo<T extends ConCodigo>(item:T):void{
+	console.log(`Codigo: ${item.codigo}`);
+}
+
+const proyecto = {
+	//codigo: "PV-001";
+	titulo: "Capacitacion digital"
+}
+*/
