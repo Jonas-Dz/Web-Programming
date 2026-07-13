df5dacfed27989a679e1804fefc05e5c41b96aa1
Jonathan Díaz <jonathandz2002@hotmail.com>
2026-07-13 07:59:00 -0500

    Ejecucion Op Asincronas

--- 3er P/OperacionesAsincronas/index.ts
+++ 3er P/OperacionesAsincronas/index.ts
@@ -1,5 +1,6 @@
 //Simular una operacion con demora usando promesas
 
+/*
 function obtenerDatos(): Promise<string> {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -16,3 +17,32 @@ obtenerDatos().then((
 );
 
 console.log("Fin");
+
+*/
+
+interface Usuario {
+    id: number;
+    nombre: string;
+    usuario: string;
+    email: string;
+}
+
+async function obtenerUsuarios(): Promise<Usuario[]> {
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usuarios: Usuario[] = await respuesta.json();
+    return usuarios;
+}
+
+async function listarUsuarios(): Promise<void> {
+    const usuarios: Usuario[] = await obtenerUsuarios();
+    usuarios.forEach((usuarios:Usuario):void => {
+        console.log("-------------------------");
+        console.log("ID: ", usuarios.id);
+        console.log("Nombre: ", usuarios.nombre);
+        console.log("Usuario: ", usuarios.usuario);
+        console.log("Correo: ", usuarios.email);
+    }
+    );
+}
+
+listarUsuarios();
